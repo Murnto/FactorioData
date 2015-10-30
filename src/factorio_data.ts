f403212bbe1f4c6dcb5bc96e88cf6816538224ca
David <Murnto@users.noreply.github.com>
2015-10-30 19:04:32 +1100

    Refactor factorio_data.ts

--- src/factorio_data.ts
+++ src/factorio_data.ts
@@ -1,13 +1,13 @@
 import ConfigData = require('./config_data');
 import fs = require('fs');
 
+var PACK_DIR = 'public/pack';
+
 module FactorioData {
     var packs = {};
     export var packInfos = [];
 
-    function loadPacks() {
-        var PACK_DIR = 'public/pack';
-
+    function loadPacks():void {
         var dirs = fs.readdirSync(PACK_DIR);
         for (var i in dirs) {
             var dir = dirs[i];
@@ -15,24 +15,19 @@ module FactorioData {
 
             if (stats.isDirectory() && dir != '.git') {
                 var pack = loadPack(dir, dir);
+
                 packInfos.push(pack.info);
-            }
-        }
-
-        for (var i in dirs) {
-            var dir = dirs[i];
-            var stats = fs.lstatSync(PACK_DIR + '/' + dir);
-
-            if (stats.isFile() && dir.indexOf('.') === -1) {
+            } else if (stats.isFile() && dir.indexOf('.') === -1) { // perform linking of packs (eg. default)
                 var content = fs.readFileSync(PACK_DIR + '/' + dir, 'utf8').trim();
                 packs[dir] = packs[content];
+
                 console.log('Map', dir, 'to', content);
             }
         }
     }
 
-    function loadPack(packid, name) {
-        var pack = new ConfigData(packid, name, 'public/pack/' + packid);
+    function loadPack(packid, name):ConfigData {
+        var pack = new ConfigData(packid, name, PACK_DIR + '/' + packid);
         packs[packid] = pack;
         return pack;
     }
