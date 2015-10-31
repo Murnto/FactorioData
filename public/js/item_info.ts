5e6f1e8cd1f04b6bde626673e3a2c684a75c5fd7
David <Murnto@users.noreply.github.com>
2015-10-31 16:05:20 +1100

    Don't show popover if user has stopped hovering

--- public/js/item_info.ts
+++ public/js/item_info.ts
@@ -1,17 +1,26 @@
 /// <reference path="../../typings/jquery/jquery.d.ts" />
 
 declare var modpack;
-function _hoverFunc() {
+function _hoverFunc(e) {
     var el = $(this);
-    if (!el.data('loaded')) {
-        el.data('loaded', true);
-        $.get('/pack/' + modpack + '/api/popup/' + el.data('item-type') + '/' + el.data('item-name'), function(response) {
-            var po = (<any> el.unbind('hover')).popover({
-                content: response,
-                html: true
+    if (e.type == 'mouseenter') {
+        // still show the popover if the user left and returned hover
+        el.data('cancelled', false);
+
+        if (!el.data('loaded')) {
+            el.data('loaded', true);
+
+            $.get('/pack/' + modpack + '/api/popup/' + el.data('item-type') + '/' + el.data('item-name'), function (response) {
+                var po = (<any> el.unbind('hover')).popover({
+                    content: response,
+                    html: true
+                });
+                po.popover(el.data('cancelled') ? 'hide' : 'show');
             });
-            po.popover('show');
-        });
+        }
+    } else if (e.type == 'mouseleave') {
+        // don't show the popover when it loads
+        el.data('cancelled', true);
     }
 }
 $(document).ready(function() {
