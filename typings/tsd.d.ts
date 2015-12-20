324f080e370724a1a758bbbf1db4078aff39d763
David <Murnto@users.noreply.github.com>
2015-12-20 13:09:31 +1100

    Update typings

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="express/express.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="async/async.d.ts" />
+/// <reference path="ini/ini.d.ts" />
+/// <reference path="mime/mime.d.ts" />
+/// <reference path="morgan/morgan.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
